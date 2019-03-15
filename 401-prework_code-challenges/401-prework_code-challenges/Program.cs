2422cfec8a2a23a78eb217591faef1a38dd01566
amjcurtis <curtis.andrewm@gmail.com>
2019-03-14 22:09:03 -0700

    Add project files for code challenge console app

---
+++ 401-prework_code-challenges/401-prework_code-challenges/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _401_prework_code_challenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the 401 prework code challenges!");
+            Console.ReadLine();
+
+            // Call method for challenge 1
+
+            // Call method for challenge 2
+
+            // Call method for challenge 3
+
+            // Call method for challenge 4
+
+        }
+
+        // Solution for challenge 1
+
+        // Solution for challenge 2
+
+        // Solution for challenge 3
+
+        // Solution for challenge 4
+
+    }
+}
